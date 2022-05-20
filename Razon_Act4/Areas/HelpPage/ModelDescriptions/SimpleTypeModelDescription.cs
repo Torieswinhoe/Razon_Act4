2a1612e9481f250782c333c832a0f173d9267e20
admin <admin@DESKTOP-3TVDJ29>
2022-05-20 20:53:45 +0800

    Add project files.

---
+++ Razon_Act4/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Razon_Act4.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Razon_Act4/Controllers/ScheduleController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Razon_Act4.Models;
+
+namespace Razon_Act4.Controllers
+{
+    public class ScheduleController : ApiController
+    {
+            [HttpGet]
+            public HttpResponseMessage Index()
+            {
+                try
+                {
+                    sched_dbEntities2 db = new sched_dbEntities2();
+                    var schedules = db.Schedules;
+                    var response = Request.CreateResponse<IEnumerable<Schedule>>(HttpStatusCode.OK, schedules);
+                    return response;
+                }
+                catch (Exception ex)
+                {
+                    var errorresponse = Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+                    return errorresponse;
+                }
+            }
+            [HttpGet]
+            public HttpResponseMessage Index(int id)
+            {
+                try
+                {
+                sched_dbEntities2 db = new sched_dbEntities2();
+                    var schedule = db.Schedules.Where(m => m.Schedule1==id).FirstOrDefault();
+                    if (schedule == null)
+                        throw new Exception("Schedule not Found");
+                    var response = Request.CreateResponse<Schedule>(HttpStatusCode.OK, schedule);
+                    return response;
+                }
+                catch (Exception ex)
+                {
+                    var errorresponse = Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+                    return errorresponse;
+                }
+            }
+
+        }
+    }
+
